e2e61c8312ebcace4e978cabb6901277006f4037
Alex Rahgozar <AlexRahgozar@gmail.com>
2018-10-24 01:12:41 -0700

    fixed production routes

--- server/index.js
+++ server/index.js
@@ -33,26 +33,19 @@ billingRoutes(app);
 
 // require("./../routes/authRoutes")(app);
 // require("./../routes/billingRoutes")(app);
-
 if (process.env.NODE_ENV === "production") {
-  // if statements:
-  // making sure express will serve production files: main.js, main.css files!
-  // console.log("hereeeeeeeee124");
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
 
-  // making sure express will serve the index.html file if it doesn't
-  // recognize the route.
-  var path = require("path");
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
   app.get("*", (req, res) => {
-    const index = path.resolve(__dirname, "client", "build", "index.html");
-    res.sendFile(index);
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-
-  // const path = require("path");
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
 }
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
   console.log(`hello i am listening at port ${PORT}`);
